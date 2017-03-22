bc9e12833759b1e851a6fa7a9dcef9e94a894fd4
jayhe <1054134542@qq.com>
2017-03-22 11:45:39 +0800

    add files

---
+++ ModuleB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleB
+//
+//  Created by hechao on 17/3/22.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ upload.sh
@@ -0,0 +1,1 @@
+pod repo push PrivatePods ModuleB.podspec --verbose --allow-warnings
